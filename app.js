d2cfcb73ebff9fbb00fe9f6eaf458f0cfeb0cb04
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2018-02-18 15:05:21 -0800

    initial passport

--- app.js
+++ app.js
@@ -5,6 +5,118 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var LocalStrategy = require('passport-local')
+var flash = require("connect-flash");
+
+var app = express();
+
+// Configuring Passport
+
+app.use(flash());
+
+var isValidPassword = function(user, password){
+  return bCrypt.compareSync(password, user.password);
+}
+
+// Generates hash using bCrypt
+var createHash = function(password){
+  return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+ }
+
+var passport = require('passport');
+var expressSession = require('express-session');
+var index = require('./routes/index')(passport)
+const User = require('./models/user')
+
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// passport/login.js
+passport.use('login', new LocalStrategy({
+  passReqToCallback : true
+},
+function(req, username, password, done) { 
+  // check in mongo if a user with username exists or not
+  User.findOne({ 'username' :  username }, 
+    function(err, user) {
+      // In case of any error, return using the done method
+      if (err)
+        return done(err);
+      // Username does not exist, log error & redirect back
+      if (!user){
+        console.log('User Not Found with username '+username);
+        return done(null, false, 
+              req.flash('message', 'User Not found.'));                 
+      }
+      // User exists but wrong password, log the error 
+      if (!isValidPassword(user, password)){
+        console.log('Invalid Password');
+        return done(null, false, 
+            req.flash('message', 'Invalid Password'));
+      }
+      // User and password both match, return user from 
+      // done method which will be treated like success
+      return done(null, user);
+    }
+  );
+}));
+
+passport.use('signup', new LocalStrategy({
+  passReqToCallback : true
+},
+function(req, username, password, done) {
+  findOrCreateUser = function(){
+    // find a user in Mongo with provided username
+    User.findOne({'username':username},function(err, user) {
+      // In case of any error return
+      if (err){
+        console.log('Error in SignUp: '+err);
+        return done(err);
+      }
+      // already exists
+      if (user) {
+        console.log('User already exists');
+        return done(null, false, 
+           req.flash('message','User Already Exists'));
+      } else {
+        // if there is no user with that email
+        // create the user
+        var newUser = new User();
+        // set the user's local credentials
+        newUser.username = username;
+        newUser.password = createHash(password);
+        newUser.email = req.param('email');
+        newUser.firstName = req.param('firstName');
+        newUser.lastName = req.param('lastName');
+
+        // save the user
+        newUser.save(function(err) {
+          if (err){
+            console.log('Error in Saving user: '+err);  
+            throw err;  
+          }
+          console.log('User Registration succesful');    
+          return done(null, newUser);
+        });
+      }
+    });
+  };
+   
+  // Delay the execution of findOrCreateUser and execute 
+  // the method in the next tick of the event loop
+  process.nextTick(findOrCreateUser);
+}));
 
 //Database set up
 let MONGO_URL
@@ -25,15 +137,12 @@ const db = mongoose.connection;
 db.on("error", error => console.log("Database Error:", error));
 db.once("open", () => console.log("Mongoose connection successful."));
 
-var index = require('./routes/index');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// un-comment after placing your favicon in /public
+// un-comment after placing favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
