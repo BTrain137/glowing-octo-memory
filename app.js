57c94ff2df251c3f1c9c5b7230e6fb39ecc9b9e2
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2018-02-17 19:37:14 -0800

    preparing heroku mongodb

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 //Database set up
-var dbConfig = require('./db.js');
-var mongoose = require('mongoose');
-mongoose.connect(dbConfig.url);
+let MONGO_URL
+const MONGO_LOCAL_URL = require('./db.js')
+
+if (process.env.MONGODB_URI) {
+  // heroku 
+	mongoose.connect(process.env.MONGODB_URI)
+	//MONGO_URL = process.env.MONGODB_URI
+} 
+else {
+  // local mongo url
+	mongoose.connect(MONGO_LOCAL_URL.url) 
+	//MONGO_URL = MONGO_LOCAL_URL.url
+}
+
+const db = mongoose.connection;
+db.on("error", error => console.log("Database Error:", error));
+db.once("open", () => console.log("Mongoose connection successful."));
 
 var index = require('./routes/index');
 
@@ -18,7 +33,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// un-comment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
