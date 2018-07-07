4ede1d1c077f87610795d7e93ab1001a9c54e1a1
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2018-07-06 21:19:32 -0700

    env

--- database/index.js
+++ database/index.js
@@ -1,16 +1,7 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 //NoSQL mongo database
-const MONGO_LOCAL_URL = 'mongodb://localhost/passport'
-
-if (process.env.MONGODB_URI) {
-  // heroku 
-	mongoose.connect(process.env.MONGODB_URI)
-} 
-else {
-  // local mongo url
-	mongoose.connect(MONGO_LOCAL_URL) 
-}
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/passport")
 
 const db = mongoose.connection;
 db.on("error", error => console.log("Database Error:", error));
