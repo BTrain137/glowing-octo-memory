7ece7079df78aa9cdc66578fbdba79c4e7e60144
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2018-07-17 16:29:19 -0700

    express-cc

--- service/passport.js
+++ service/passport.js
@@ -13,10 +13,14 @@ const isValidPassword = function(user, password){
    }
 
 passport.serializeUser(function(user, done) {
+    console.log("===========serializeUser============");
+    
     done(null, user._id);
   });
   
   passport.deserializeUser(function(id, done) {
+    console.log("===========deserializeUser============");
+
     User.findById(id, function(err, user) {
       done(err, user);
     });
