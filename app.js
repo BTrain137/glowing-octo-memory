76ef06d28ce22b422d236ffb3872addb696f65b5
Bryan Tran <btran@zr-mac-21.local>
2018-07-18 12:05:10 -0700

    set up session for passport

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  expressValidator = require('express-validator'),
+  session = require('express-session'); 
 
 var app = express();
 
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,19 +25,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  // cookie: { secure: true }
+}))
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -65,8 +72,8 @@ filenames.forEach(function (filename) {
   hbs.registerPartial(name, template);
 });
 
-hbs.registerHelper('json', function(context) {
-    return JSON.stringify(context, null, 2);
+hbs.registerHelper('json', function (context) {
+  return JSON.stringify(context, null, 2);
 });
 
 
